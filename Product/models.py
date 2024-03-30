0e822ec9a18a4adae09ece7c17c3dd08685447a7
Abanoubmedhatseif <abanoub.medhat.seif@gmail.com>
2024-03-30 23:14:43 +0200

    added product model

--- Product/models.py
+++ Product/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model): 
+    name = models.CharField(max_length=50) 
+    description = models.TextField() 
+    price = models.DecimalField(max_digits=5, decimal_places=2) 
+    image = models.ImageField(upload_to='products/%y/%m/%d/') 
+    # category = models.ForeignKey(Categories, on_delete=models.PROTECT) 
+    active=models.BooleanField(default=True)
+    stock = models.IntegerField(default=0)
+
+
+    def __str__(self): 
+        return self.name
