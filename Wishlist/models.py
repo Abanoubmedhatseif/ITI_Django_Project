11e969f8805f58e574cbbacc100c84703d51c43b
Steven Adel <stevenadel.dev@gmail.com>
2024-03-31 17:26:11 +0200

    Add wishlist model

--- Wishlist/models.py
+++ Wishlist/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from User.models import User
+from Product.models import Product
 
-# Create your models here.
+
+class Wishlist(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    products = models.ManyToManyField(Product)
+
+    def __str__(self):
+        return f"Wishlist for {self.user.username}"
