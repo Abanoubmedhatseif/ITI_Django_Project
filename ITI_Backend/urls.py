846bc35b878c2596500a1c5b65a5f02d8057ad10
esraamagddi <esraamagdy20133@gmail.com>
2024-04-02 23:48:46 +0200

    commiting changes before pulling cart

--- ITI_Backend/urls.py
+++ ITI_Backend/urls.py
@@ -22,5 +22,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('User.urls')),
     path('', include('Product.urls')),
+    path('', include('Orders.urls')),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
