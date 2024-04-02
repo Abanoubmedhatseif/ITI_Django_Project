f968399506f9196775d1ae389f565648c82cae14
yossrmohammed <yossr.khatab5@gmail.com>
2024-04-02 22:50:23 +0200

    cartLogic

--- Cart/views.py
+++ Cart/views.py
@@ -1,3 +1,57 @@
-from django.shortcuts import render
+from rest_framework.decorators import (
+    api_view,
+    authentication_classes,
+    permission_classes,
+)
+from rest_framework.response import Response
+from rest_framework.authentication import SessionAuthentication
+from rest_framework.permissions import IsAuthenticated
+from rest_framework import status
+from ..Product.models import Product
 
-# Create your views here.
+
+@api_view(["GET", "POST", "PUT", "DELETE"])
+@authentication_classes([SessionAuthentication])
+@permission_classes([IsAuthenticated])
+def cart(request):
+    if request.method == "GET":
+
+        cart_data = request.session.get("cart", {})
+        return Response(cart_data)
+
+    elif request.method == "POST":
+        product_id = request.data.get("product_id")
+
+        try:
+            product = Product.objects.get(pk=product_id)
+        except Product.DoesNotExist:
+            return Response(
+                {"error": "Product not found"}, status=status.HTTP_404_NOT_FOUND
+            )
+        cart_data = product
+
+        cart_session = request.session.get("cart", {})
+        cart_session.update({product_id: cart_data})
+        request.session["cart"] = cart_session
+
+        return Response(cart_session, status=status.HTTP_201_CREATED)
+
+    elif request.method == "PUT":
+
+        cart_data = request.session.get("cart", {})
+        updated_item = request.data
+        cart_data.update(updated_item)
+        request.session["cart"] = cart_data
+        return Response(cart_data)
+
+    elif request.method == "DELETE":
+        cart_data = request.session.get("cart", {})
+        item_to_remove = request.data.get("item_id")
+        if item_to_remove in cart_data:
+            del cart_data[item_to_remove]
+            request.session["cart"] = cart_data
+            return Response(cart_data, status=status.HTTP_204_NO_CONTENT)
+        else:
+            return Response(
+                {"error": "Item not found in cart"}, status=status.HTTP_404_NOT_FOUND
+            )
--- ITI_Backend/settings.py
+++ ITI_Backend/settings.py
@@ -13,7 +13,8 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 from pathlib import Path
 from dotenv import load_dotenv
 import os
-dotenv_path = Path(__file__).resolve().parent.parent / 'dot.env'
+
+dotenv_path = Path(__file__).resolve().parent.parent / "dot.env"
 
 load_dotenv(dotenv_path)
 
@@ -26,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-yuh&y4g053-&x&hs9nw8j+rgzi^!&0-q=c8a=3+d#h-f8@)dxh'
+SECRET_KEY = "django-insecure-yuh&y4g053-&x&hs9nw8j+rgzi^!&0-q=c8a=3+d#h-f8@)dxh"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,74 +38,74 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'rest_framework',
-    'User',
-    'Product',
-    'Categories',
-    'Cart',
-    'Orders',
-    'Wishlist',
-    'rest_framework.authtoken'
-    
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "rest_framework",
+    "User",
+    "Product",
+    "Categories",
+    "Cart",
+    "Orders",
+    "Wishlist",
+    "rest_framework.authtoken",
 ]
-AUTH_USER_MODEL = 'User.CustomUser'
-REST_FRAMEWORK={
-        'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.TokenAuthentication',
+AUTH_USER_MODEL = "User.CustomUser"
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.TokenAuthentication",
+        "rest_framework.authentication.TokenAuthentication",
+        "rest_framework.authentication.SessionAuthentication",
     ],
 }
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-ROOT_URLCONF = 'ITI_Backend.urls'
+ROOT_URLCONF = "ITI_Backend.urls"
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
             ],
         },
     },
 ]
 
-WSGI_APPLICATION = 'ITI_Backend.wsgi.application'
+WSGI_APPLICATION = "ITI_Backend.wsgi.application"
 
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.getenv('MYSQLDATABASE'),
-        'USER': os.getenv('MYSQLUSER', 'root'),
-        'PASSWORD': os.getenv('MYSQLPASSWORD'),
-        'HOST': os.getenv('MYSQLHOST'),   
-        'PORT': os.getenv('MYSQLPORT'),   
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": os.getenv("MYSQLDATABASE"),
+        "USER": os.getenv("MYSQLUSER", "root"),
+        "PASSWORD": os.getenv("MYSQLPASSWORD"),
+        "HOST": os.getenv("MYSQLHOST"),
+        "PORT": os.getenv("MYSQLPORT"),
     }
 }
-
 
 
 # Password validation
@@ -112,16 +113,16 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
 
@@ -129,9 +130,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = "UTC"
 
 USE_I18N = True
 
@@ -141,12 +142,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = "static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
- 
-MEDIA_ROOT = os.path.join(BASE_DIR , 'media')
-MEDIA_URL = '/media/'
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_URL = "/media/"
