2f231aa8a6c929868dde87ca276e42317b7f43dc
esraamagddi <esraamagdy20133@gmail.com>
2024-04-05 22:06:45 +0200

    add checkout using session endpoint

--- Orders/urls.py
+++ Orders/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import order_list, order_detail, confirm_order
+from .views import order_list, order_detail, confirm_order,create_checkout_session
 
 urlpatterns = [
     path('confirm-order/', confirm_order, name='confirm-order'),#user
     path('orders/', order_list, name='order-list'),#user
     path('orders/<str:pk>/', order_detail, name='order-detail'),#user--> get order_detail
+    path('checkout/', create_checkout_session, name='create-checkout-session'),
 ]
