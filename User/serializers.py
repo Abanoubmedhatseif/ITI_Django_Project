9962fa4c3c2d9f776fd011fe33a49f564bd93eb7
Steven Adel <stevenadel.dev@gmail.com>
2024-04-06 22:58:41 +0200

    Apply formatting

--- User/serializers.py
+++ User/serializers.py
@@ -1,26 +1,33 @@
 from rest_framework import serializers
 from .models import CustomUser
 
+
 class RegisterSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomUser
-        fields = ['username','first_name','last_name','password','email','shipping_address','profile_photo']
-        extra_kwargs = {'password': {'write_only': True}}
+        fields = [
+            "username",
+            "first_name",
+            "last_name",
+            "password",
+            "email",
+            "shipping_address",
+            "profile_photo",
+        ]
+        extra_kwargs = {"password": {"write_only": True}}
 
     def create(self, validated_data):
-        photo = validated_data.pop('profile_photo', None)
+        photo = validated_data.pop("profile_photo", None)
         user = CustomUser.objects.create(
-            username=validated_data['username'],
-            first_name=validated_data['first_name'],
-            last_name=validated_data['last_name'],
-            email=validated_data['email'],
-            shipping_address=validated_data['shipping_address'],
-            
+            username=validated_data["username"],
+            first_name=validated_data["first_name"],
+            last_name=validated_data["last_name"],
+            email=validated_data["email"],
+            shipping_address=validated_data["shipping_address"],
         )
-        user.set_password(validated_data['password'])
+        user.set_password(validated_data["password"])
         if photo:
             user.profile_photo = photo
-            
+
         user.save()
         return user
-    
