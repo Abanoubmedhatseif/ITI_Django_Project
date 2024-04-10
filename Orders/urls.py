04b0526574bcab39ada179508794b4efc2654c5b
esraamagddi <esraamagdy20133@gmail.com>
2024-04-10 06:20:54 +0200

    Modifications for frontend integration

--- Orders/urls.py
+++ Orders/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import order_list, order_detail, confirm_order, create_checkout_session
+from .views import order_list, order_detail, create_checkout_session
 
 urlpatterns = [
-    path('confirm-order/', confirm_order, name='confirm-order'),  
+    # path('confirm-order/', confirm_order, name='confirm-order'),  
     path('orders/', order_list, name='order-list'),  
     path('orders/<str:pk>/', order_detail, name='order-detail'),  
     path('checkout/', create_checkout_session, name='create-checkout-session'),  
--- Orders/views.py
+++ Orders/views.py
@@ -11,9 +11,8 @@ from rest_framework.authentication import SessionAuthentication, TokenAuthentica
 import stripe
 from django.conf import settings
 from django.core.mail import send_mail
+from django.shortcuts import redirect
 
-cash_payment_classes = [SessionAuthentication, TokenAuthentication]
-card_payment_classes = [SessionAuthentication, TokenAuthentication]
 default_payment_method = "cash"
 
 @api_view(['GET', 'POST'])
@@ -24,67 +23,30 @@ def order_list(request):
         orders = Order.objects.filter(user=request.user)
         serializer = OrderSerializer(orders, many=True)
         return Response(serializer.data)
-    elif request.method == 'POST':
-        payment_method = request.data.get('payment_method', default_payment_method)
-        if payment_method == 'card':
-            return create_checkout_session(request)
-        elif payment_method == 'cash':
-            return confirm_order(request)
-        else:
-            return Response({'error': 'Invalid payment method'}, status=status.HTTP_400_BAD_REQUEST)
-
-
-@api_view(["POST"])
-@authentication_classes(cash_payment_classes)
+    
+def send_order_confirmation_email(user_email):
+    subject = 'Order Confirmation'
+    message = "Your order has been successfully placed."
+    from_email = settings.DEFAULT_FROM_EMAIL
+    to_email = [user_email]
+    send_mail(subject, message, from_email, to_email)
+    
+stripe.api_key = settings.STRIPE_SECRET_KEY
 @authentication_classes([TokenAuthentication])
 @permission_classes([IsAuthenticated])
-def confirm_order(request: Request):
-    try:
-        cart_id = request.data.get("cart_id")
-        if not cart_id:
-            return Response(
-                {"error": "Cart ID is required"}, status=status.HTTP_400_BAD_REQUEST
-            )
-
-        cart_items = CartItem.objects.filter(cart_id=cart_id)
-
-        total_price = sum(cart_item.product.price * cart_item.quantity for cart_item in cart_items)
-
-        with transaction.atomic():
-            order = Order.objects.create(
-                user=request.user,
-                total_price=total_price,
-            )
-            for cart_item in cart_items:
-                order.products.add(cart_item.product)
-
-        cart_items.delete()
-        
-        subject = 'Order Confirmation'
-        message = f"Your order  has been successfully placed."
-        from_email = settings.DEFAULT_FROM_EMAIL
-        to_email = [request.user.email]  
-        print(request.user.email)
-        send_mail(subject, message, from_email, to_email)
-        
-        return Response(
-            {"message": "Order created successfully"},
-            status=status.HTTP_201_CREATED,
-        )
-    except Exception as e:
-        return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-
-
 @api_view(["POST"])
-@authentication_classes(card_payment_classes)
-@authentication_classes([TokenAuthentication])
-@permission_classes([IsAuthenticated])
 def create_checkout_session(request: Request):
     try:
         cart_id = request.data.get("cart_id")
         if not cart_id:
             return Response(
                 {"error": "Cart ID is required"}, status=status.HTTP_400_BAD_REQUEST
+            )
+
+        payment_method = request.data.get("payment_method")
+        if payment_method not in ['cash', 'card']:
+            return Response(
+                {"error": "Invalid payment method"}, status=status.HTTP_400_BAD_REQUEST
             )
 
         cart_items = CartItem.objects.filter(cart_id=cart_id)
@@ -104,18 +66,38 @@ def create_checkout_session(request: Request):
             })
             total_price += cart_item.product.price * cart_item.quantity
 
-        checkout_session = stripe.checkout.Session.create(
-            payment_method_types=["card"],
-            line_items=line_items,
-            mode="payment",
-            success_url=settings.SITE_URL+'/?success=true&session_id={CHECKOUT_SESSION_ID}',
-            cancel_url=settings.SITE_URL+'/?cancel=true',
-        )
-        cart_items.delete()
-        return Response({'redirect_url': checkout_session.url}, status=status.HTTP_201_CREATED)
+        if payment_method == 'cash':
+            with transaction.atomic():
+                order = Order.objects.create(
+                    user=request.user,
+                    total_price=total_price,
+                )
+                for cart_item in cart_items:
+                    order.products.add(cart_item.product)
+                    
+            send_order_confirmation_email(request.user.email)
+
+            cart_items.delete()
+
+            return Response(
+                {"message": "Order created successfully"},
+                status=status.HTTP_201_CREATED,
+            )
+        else:
+            checkout_session = stripe.checkout.Session.create(
+                payment_method_types=["card"],
+                line_items=line_items,
+                mode="payment",
+                success_url=settings.SITE_URL+'/?success=true&session_id={CHECKOUT_SESSION_ID}',
+                cancel_url=settings.SITE_URL+'/?cancel=true',
+            )
+            
+            send_order_confirmation_email(request.user.email)
+
+            cart_items.delete()
+            return Response({'url': checkout_session.url}, status=status.HTTP_201_CREATED)
     except stripe.error.StripeError as e:
         return Response({"error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
-
 
 @api_view(['GET', 'PUT', 'PATCH', 'DELETE'])
 @authentication_classes([TokenAuthentication])
