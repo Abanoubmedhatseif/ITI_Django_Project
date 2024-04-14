c629c2265e71455fc7559dbf5dead86e1907ce97
yossrmohammed <yossr.khatab5@gmail.com>
2024-04-14 03:56:19 +0200

    finaaaal

--- ITI_Backend/settings.py
+++ ITI_Backend/settings.py
@@ -173,7 +173,8 @@ CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
-    "https://iti-django-project-vsgz.onrender.com",
+    "https://iti-django-project-vsgz.otnrender.com",
+    "https://go-cart.vercel.app",
 ]
 
 RENDER_EXTERNAL_HOSTNAME = os.environ.get("RENDER_EXTERNAL_HOSTNAME")
